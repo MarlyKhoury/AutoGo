d1c029d9d24fd1413ae14a60889dbcb92b0d677b
MarlyKhoury <marlykh625@gmail.com>
2022-03-10 14:20:29 +0200

    added the create new token API

--- AutoGo-Backend/app/Http/Controllers/AuthController.php
+++ AutoGo-Backend/app/Http/Controllers/AuthController.php
@@ -88,5 +88,19 @@ class AuthController extends Controller
     public function userProfile() {
         return response()->json(auth()->user());
     }
-   
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function createNewToken($token){
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user' => auth()->user()
+        ]);
+    }
 }
