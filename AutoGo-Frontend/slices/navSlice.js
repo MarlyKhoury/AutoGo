7ac30cc2d0822aba4123ab10ff47d24c446149fe
MarlyKhoury <marlykh625@gmail.com>
2022-03-16 14:40:34 +0200

    connected the navSlice to the store component

--- AutoGo-Frontend/slices/navSlice.js
+++ AutoGo-Frontend/slices/navSlice.js
@@ -31,3 +31,5 @@ export const {setOrigin, setDestination, setTravelTimeInformation} = navSlice.ac
 export const selectOrigin = (state) => state.nav.origin;
 export const selectDestination = (state) => state.nav.destination;
 export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation;
+
+export default navSlice.reducer;
