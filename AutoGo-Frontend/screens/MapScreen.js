ca9b645860908c7257de21ddb35fd6ff34ed8869
MarlyKhoury <marlykh625@gmail.com>
2022-03-16 23:13:19 +0200

    installed the react native maps

--- AutoGo-Frontend/screens/MapScreen.js
+++ AutoGo-Frontend/screens/MapScreen.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native"; 
 import tw from 'tailwind-react-native-classnames';
-
+import Map from "../components/Map";
 
 const MapScreen = () => {
     return (
         <View>
-            <Text>Here is the map stuff</Text>
-            <View style={tw`h-1/2`}></View>
+            <View style={tw`h-1/2`}>
+                <Map />
+            </View>
 
             <View style={tw`h-1/2`}></View>
         </View>
