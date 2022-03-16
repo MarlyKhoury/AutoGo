28d8fc0ce8813fa403e7f3b6f44b605f4b1a3fe1
MarlyKhoury <marlykh625@gmail.com>
2022-03-16 22:40:11 +0200

    installed the tailwind package

--- AutoGo-Frontend/screens/MapScreen.js
+++ AutoGo-Frontend/screens/MapScreen.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native"; 
+import tw from 'tailwind-react-native-classnames';
+
 
 const MapScreen = () => {
     return (
         <View>
             <Text>Here is the map stuff</Text>
+            <View></View>
+
+            <View></View>
         </View>
     );
 };
+
+export default MapScreen;
+
+const styles = StyleSheet.create({});
