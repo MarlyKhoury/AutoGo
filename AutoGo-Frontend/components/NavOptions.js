71201419bc407fffcf2a9271c2b4c4c9acb61601
MarlyKhoury <marlykh625@gmail.com>
2022-03-19 13:43:19 +0200

    building the NavOptions component

---
+++ AutoGo-Frontend/components/NavOptions.js
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const data = [
+{
+    id: "123",
+    title: "Get a Ride",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen",
+},
+{
+    id: "456",
+    title: "Order Food",
+    image: "https://links.papareact.com/28w",
+    screen: "EatScreen",
+},
+];
+
+const NavOptions = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  )
+}
+
+export default NavOptions
+
+const styles = StyleSheet.create({})
--- AutoGo-Frontend/screens/HomeScreen.js
+++ AutoGo-Frontend/screens/HomeScreen.js
@@ -1,11 +1,26 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames';
+import NavOptions from '../components/NavOptions';
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
-      <Text style={tw`text-red-500 p-10`}>I am the HomeScreen</Text>
+      <View style={tw`p-5`}>
+          <Image 
+             style={{
+                 width:100, 
+                 height:100, 
+                 resizeMode:'contain',
+             }}
+             source={{
+                 uri: "https://links.papareact.com/gzs",
+             }}
+          
+          
+          />
+          <NavOptions />
+      </View>
     </SafeAreaView>
   )
 }
