d4965c84671379375ca45c4d93e8d3f03d7cb234
MarlyKhoury <marlykh625@gmail.com>
2022-03-19 18:29:28 +0200

    set up the auto complete api

--- AutoGo-Frontend/screens/HomeScreen.js
+++ AutoGo-Frontend/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 const HomeScreen = () => {
   return (
@@ -17,8 +19,28 @@ const HomeScreen = () => {
                  uri: "https://links.papareact.com/gzs",
              }}
           
-          
           />
+
+          <GooglePlacesAutocomplete
+              placeholder='Where From?'
+              styles={{
+                container: {
+                  flex:0,
+                },
+                textInput:{
+                  fontSize:18,
+                }
+
+              }}
+              query= {{
+                key: GOOGLE_MAPS_APIKEY,
+                language:'en'
+              }}
+              nearbyPlacesAPI='GooglePlacesSearch'
+              debounce={400}
+           
+           
+           />
           <NavOptions />
       </View>
     </SafeAreaView>
