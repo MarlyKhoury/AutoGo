21885f4f99d563384f35c36425456239b14ed1e2
MarlyKhoury <marlykh625@gmail.com>
2022-03-19 20:42:38 +0200

    added a where to and a debounce

--- AutoGo-Frontend/components/NavigateCard.js
+++ AutoGo-Frontend/components/NavigateCard.js
@@ -1,13 +1,23 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
-import React from 'react'
-import tw from 'tailwind-react-native-classnames'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import React from 'react';
+import tw from 'tailwind-react-native-classnames';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 
 const NavigateCard = () => {
   return (
     <SafeAreaView style={tw`bg-white flex-1`}>
       <Text style={tw`text-center py-5 text-xl`}>Good Morning</Text>
-      <View style={tw`border-t border-gray-200 flex-shrink`}></View>
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
+          <View>
+             <GooglePlacesAutocomplete 
+                placeholder='Where To?'
+                nearbyPlacesAPI='GooglePlacesSearch'
+                debounce={400}             
+             />
+          </View>
+      </View>
     </SafeAreaView>
   )
 }
