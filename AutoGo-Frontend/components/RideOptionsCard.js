3de75052cb61017b8abf08c3b1c3a9e37fae9d94
MarlyKhoury <marlykh625@gmail.com>
2022-03-20 01:00:55 +0200

    added test pictures

--- AutoGo-Frontend/components/RideOptionsCard.js
+++ AutoGo-Frontend/components/RideOptionsCard.js
@@ -5,7 +5,30 @@ import { Icon } from 'react-native-elements'
 import { useNavigation } from '@react-navigation/native'
 
 
+const data = [
+    {
+        id: "Uber-X-123",
+        title: "UberX",
+        multiplier: 1,
+        image: "https://links.papareact.com/3pn",
 
+    },
+    {
+        id: "Uber-XL-456",
+        title: "Uber XL",
+        multiplier: 1.2,
+        image: "https://links.papareact.com/5w8",
+
+    },
+    {
+        id: "Uber-LUX-789",
+        title: "Uber LUX",
+        multiplier: 1.75,
+        image: "https://links.papareact.com/7pf",
+
+    },
+
+]
 const RideOptionsCard = () => {
     const navigation = useNavigation();
 
