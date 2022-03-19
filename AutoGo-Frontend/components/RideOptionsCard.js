b77fcb81469ab866dce493054da8d01367719e3c
MarlyKhoury <marlykh625@gmail.com>
2022-03-20 00:40:15 +0200

    added style to the back arrow

--- AutoGo-Frontend/components/RideOptionsCard.js
+++ AutoGo-Frontend/components/RideOptionsCard.js
@@ -9,9 +9,8 @@ const RideOptionsCard = () => {
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
         <View>
-            <TouchableOpacity>
+            <TouchableOpacity style={tw`absolute top-3 left-5 p-3 rounded-full`}>
                 <Icon name="chevron-left" type="fontawesome" />
-
             </TouchableOpacity>
       <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
         </View>
