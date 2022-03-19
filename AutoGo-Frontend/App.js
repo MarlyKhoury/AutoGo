78b547647e8a7fc950392d5331fbe08497b04d49
MarlyKhoury <marlykh625@gmail.com>
2022-03-19 23:23:47 +0200

    removed the stylesheet from App.js

--- AutoGo-Frontend/App.js
+++ AutoGo-Frontend/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import MapScreen from "./screens/MapScreen";
@@ -37,11 +37,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
