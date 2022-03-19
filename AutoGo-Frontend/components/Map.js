81594a3659bcdfd158c5e9bc5ea6049dc03c9496
MarlyKhoury <marlykh625@gmail.com>
2022-03-19 19:36:59 +0200

    got the origin value from the selector

--- AutoGo-Frontend/components/Map.js
+++ AutoGo-Frontend/components/Map.js
@@ -2,15 +2,18 @@ import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import MapView, { Marker } from 'react-native-maps';
 import tw from 'tailwind-react-native-classnames';
+import { useSelector } from 'react-redux';
+import { selectOrigin } from '../slices/navSlice';
 
 const Map = () => {
+    const origin = useSelector(selectOrigin);
   return (
     <MapView
     style={tw`flex-1`}
     mapType="mutedStandard"
     initialRegion={{
-      latitude: 37.78825,
-      longitude: -122.4324,
+      latitude: origin.location.lat,
+      longitude: origin.location.lng,
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
     }}
