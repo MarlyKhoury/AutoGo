5e4ada2fcdcb77bcbe51969575f91f1d13b82a4c
MarlyKhoury <marlykh625@gmail.com>
2022-03-19 20:18:13 +0200

    added the navigate card component

--- AutoGo-Frontend/screens/MapScreen.js
+++ AutoGo-Frontend/screens/MapScreen.js
@@ -3,15 +3,27 @@ import React from 'react';
 import tw from 'tailwind-react-native-classnames';
 import Map from '../components/Map';
 import MapView from 'react-native-maps';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const MapSreen = () => {
+  const Stack = createStackNavigator();
   return (
     <View>
       <View style={tw`h-1/2`}>
         <Map />
       </View>
 
-      <View style={tw`h-1/2`}></View>
+      <View style={tw`h-1/2`}>
+        <Stack.Navigator>
+          <Stack.Screen 
+             name = "NavigateCard"
+             component={NavigateCard}
+             options = {{
+               headerShown: false,
+             }}  
+          />
+        </Stack.Navigator>
+      </View>
     </View>
   )
 }
