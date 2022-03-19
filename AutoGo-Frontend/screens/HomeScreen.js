6bd80fb3f335c624ee3117202113e525c0e93e0d
MarlyKhoury <marlykh625@gmail.com>
2022-03-19 13:07:39 +0200

    added a Safeareaview to the homescreen component

---
+++ AutoGo-Frontend/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>I am the HomeScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
