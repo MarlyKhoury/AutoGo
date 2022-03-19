7d57d86c9a7c4a7686302e394fd13afb2e02479e
MarlyKhoury <marlykh625@gmail.com>
2022-03-19 12:49:40 +0200

    added the selectors for the origin, destination and traveltimeinfo

--- AutoGo-Frontend/slices/navSlice.js
+++ AutoGo-Frontend/slices/navSlice.js
@@ -23,3 +23,8 @@ export const navSlice = createSlice({
 });
 
 export const {setOrigin, setDestination, setTravelTimeInformation} = navSlice.actions;
+
+// Selectors
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation;
