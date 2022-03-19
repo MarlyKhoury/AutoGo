6cf01ea0cd2c1ccebbd6629d6bc0dc79be1e455e
MarlyKhoury <marlykh625@gmail.com>
2022-03-19 16:51:16 +0200

    added a navigator for the homescreen

--- AutoGo-Frontend/App.js
+++ AutoGo-Frontend/App.js
@@ -4,13 +4,27 @@ import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
     <Provider store={store}>
+      <NavigationContainer>
       <SafeAreaProvider>
-       <HomeScreen />
+        <Stack.Navigator>
+          <Stack.Screen 
+          name = "HomeScreen"
+          component={HomeScreen} 
+          options={{
+            headerShown:false,
+          }}
+          />
+        </Stack.Navigator>
       </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
