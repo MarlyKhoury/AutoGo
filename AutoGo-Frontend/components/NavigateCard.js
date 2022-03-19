3a50e138ba3c1c4ea13219527ac19bcca0772f67
MarlyKhoury <marlykh625@gmail.com>
2022-03-19 21:00:50 +0200

    added the dispatch information

--- AutoGo-Frontend/components/NavigateCard.js
+++ AutoGo-Frontend/components/NavigateCard.js
@@ -3,9 +3,13 @@ import React from 'react';
 import tw from 'tailwind-react-native-classnames';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from 'react-redux';
+import { setDestination } from '../slices/navSlice';
 
 
 const NavigateCard = () => {
+    const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white flex-1`}>
       <Text style={tw`text-center py-5 text-xl`}>Good Morning</Text>
@@ -17,7 +21,13 @@ const NavigateCard = () => {
                 fetchDetails={true}
                 returnKeyType={"search"}
                 minLength={2}
-                
+                onPress={(data, details = null) => {
+                    dispatch(setDestination({
+                        location: details.geometry.location,
+                        description: data.description,
+
+                    }))
+                }}
                 enablePoweredByContainer={false}
                 query= {{
                     key: GOOGLE_MAPS_APIKEY,
