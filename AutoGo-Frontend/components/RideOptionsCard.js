edd381b2f5c7cdff2d9c63216f5289da8597d7df
MarlyKhoury <marlykh625@gmail.com>
2022-03-20 00:54:43 +0200

    added style to the back arrow

--- AutoGo-Frontend/components/RideOptionsCard.js
+++ AutoGo-Frontend/components/RideOptionsCard.js
@@ -2,22 +2,28 @@ import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-na
 import React from 'react'
 import tw from 'tailwind-react-native-classnames'
 import { Icon } from 'react-native-elements'
+import { useNavigation } from '@react-navigation/native'
 
 
 
 const RideOptionsCard = () => {
+    const navigation = useNavigation();
+
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
         <View>
-            <TouchableOpacity style={tw`absolute top-3 left-5 p-3 rounded-full`}>
-                <Icon name="chevron-left" type="fontawesome" />
+            <TouchableOpacity 
+            onPress={() => navigation.navigate("NavigateCard")}
+            style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}
+            >
+            <Icon name="chevron-left" type="fontawesome" />
             </TouchableOpacity>
       <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
         </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default RideOptionsCard
+export default RideOptionsCard;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
