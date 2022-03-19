45978eb70a3d4bc7b09e0c7a764e39d34148a22f
MarlyKhoury <marlykh625@gmail.com>
2022-03-19 12:01:48 +0200

    installed react redux toolkit

--- AutoGo-Frontend/App.js
+++ AutoGo-Frontend/App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
+import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Let's build AutoGo!!</Text>
-      <StatusBar style="auto" />
+      
     </View>
   );
 }
