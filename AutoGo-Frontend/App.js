87223f20f6e4af6636a8f2a019ada7fdffd5ee05
MarlyKhoury <marlykh625@gmail.com>
2022-03-19 12:58:00 +0200

    created a screens folder

--- AutoGo-Frontend/App.js
+++ AutoGo-Frontend/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function App() {
   return (
-    <Provider>
+    <Provider store={store}>
        <View style={styles.container}>
           <Text>Let's build AutoGo!!</Text>
        </View>
