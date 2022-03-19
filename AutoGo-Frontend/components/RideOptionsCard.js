50466247ef55fa476df17f49804dc870e106404f
MarlyKhoury <marlykh625@gmail.com>
2022-03-20 00:36:26 +0200

    adding a back arrow icon

--- AutoGo-Frontend/components/RideOptionsCard.js
+++ AutoGo-Frontend/components/RideOptionsCard.js
@@ -1,12 +1,20 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames'
+import { Icon } from 'react-native-elements'
+
 
 
 const RideOptionsCard = () => {
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
+        <View>
+            <TouchableOpacity>
+                <Icon name="chevron-left" type="fontawesome" />
+
+            </TouchableOpacity>
       <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+        </View>
     </SafeAreaView>
   )
 }
