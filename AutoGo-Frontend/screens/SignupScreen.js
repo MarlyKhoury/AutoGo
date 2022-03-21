e20e9f4b7d7af57bced84e96c61bbad7b53519a2
MarlyKhoury <marlykh625@gmail.com>
2022-03-21 15:15:48 +0200

    styling the text inputs in the signup form

--- AutoGo-Frontend/screens/SignupScreen.js
+++ AutoGo-Frontend/screens/SignupScreen.js
@@ -9,7 +9,6 @@ import tw from 'tailwind-react-native-classnames';
 const SignUpScreen = () => {
   const navigation = useNavigation();
 
-
   const [first_name, setFirst_name] = React.useState("");
   const [last_name, setLast_name] = React.useState("");
   const [gender, setGender] = React.useState("");
@@ -21,43 +20,43 @@ const SignUpScreen = () => {
       <View>
       <Header />
       <TextInput
-      style={tw`mt-5`}
+      style={[tw`mt-3 justify-center rounded-xl`,{marginLeft:20,width:336}]}
       label="First Name"
       value={first_name}
       onChangeText={first_name => setFirst_name(first_name)}
       />
       <TextInput
-      style={tw`mt-3`}
+      style={[tw`mt-3 justify-center rounded-xl`,{marginLeft:20,width:336}]}
       label="Last Name"
       value={last_name}
       onChangeText={last_name => setLast_name(last_name)}
       />
       <TextInput
-      style={tw`mt-3`}
+      style={[tw`mt-3 justify-center rounded-xl`,{marginLeft:20,width:336}]}
       label="Gender"
       value={gender}
       onChangeText={gender => setGender(gender)}
       />
       <TextInput
-      style={tw`mt-3`}
+      style={[tw`mt-3 justify-center rounded-xl`,{marginLeft:20,width:336}]}
       label="Phone Number"
       value={phone_number}
       onChangeText={phone_number => setPhone_number(phone_number)}
       />
       <TextInput
-      style={tw`mt-3`}
+      style={[tw`mt-3 justify-center rounded-xl`,{marginLeft:20,width:336}]}
       label="Email"
       value={email}
       onChangeText={email => setEmail(email)}
       />
       <TextInput
-      style={tw`mt-3`}
+      style={[tw`mt-3 justify-center rounded-xl`,{marginLeft:20,width:336}]}
       label="Password"
       secureTextEntry
       value={password}
       onChangeText={password => setPassword(password)}
       />
-       <Button style={styles.button} icon="camera" mode="contained" onPress={() => navigation.navigate('LoginScreen')}>
+      <Button style={styles.button} icon="camera" mode="contained" onPress={() => navigation.navigate('LoginScreen')}>
         Sign Up
       </Button>
       <Text style={styles.acctext}>Already have an account?<Text>Log In</Text></Text>
@@ -71,6 +70,8 @@ const styles = StyleSheet.create({
   button:{
      backgroundColor: "#454545",
      marginTop:12,
+     width:336,
+     marginLeft:20,
   },
   acctext:{
     color: "#58BD29",
