c2fb3ed38ff9b55c0172b5e889a006ae67e19307
MarlyKhoury <marlykh625@gmail.com>
2022-03-31 00:38:03 +0300

    added a title for the navigateCard component

--- AutoGo-Frontend/components/NavigateCard.js
+++ AutoGo-Frontend/components/NavigateCard.js
@@ -27,7 +27,7 @@ const NavigateCard = () => {
   return (
       getDataUsingSimpleGetCall(),
     <SafeAreaView style={tw`bg-white flex-1`}>
-      {/* <Text style={tw`text-center py-5 text-xl`}>Hello!</Text> */}
+      <Text style={tw`text-center py-5 text-xl`}>Ride Booking</Text>
       <View style={tw`border-t border-gray-200 flex-shrink`}>
           <View >
           <GooglePlacesAutocomplete
