d7c646bb59c12f7573a6df48241c33b7a5a2c2aa
MarlyKhoury <marlykh625@gmail.com>
2022-04-03 11:12:09 +0300

    setting up the admin search bar

--- AutoGo-Frontend/components/SearchBar.js
+++ AutoGo-Frontend/components/SearchBar.js
@@ -22,23 +22,7 @@ useEffect(()=>{
     let filteredData = [];
     console.log("query ", query);
     setSearchQuery(query)
-    if(InitialData && InitialData!==undefined)
-    InitialData?.filter((item) => {
-      // console.log("item ", JSON.stringify(item.first_name));
-      // console.log("search query ", searchQuery)
-      console.log("-------------------------------")
-      if(item.first_name.toString().toLowerCase().includes(searchQuery?.toString().toLowerCase())){
-        // console.log("item ---> ", JSON.stringify(item));
-        filteredData.push(item);
-        console.log("-------------------------------2")
-
-        
-      }
-      
-    });
-    console.log(" fil data ----> ", filteredData && JSON.stringify(filteredData))
-    if(filteredData && filteredData !=undefined)
-  props.setData(filteredData);
+   
     
   };
 
